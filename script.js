a0969887736ea06d6fcafccc55d9ea5fcae6e4fb
Veilside13 <morenodaffa04@gmail.com>
2024-01-05 16:15:10 +0700

    Company Profile

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Toggle class active
+// Toggle class active hamburger menu
 
 const navbarNav = document.querySelector
 ('.navbar-nav');
@@ -8,6 +8,16 @@ const navbarNav = document.querySelector
 document.querySelector('#hamburger-menu').
 onclick = () => {
     navbarNav.classList.toggle('active');
+};
+
+// Toggle active search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
 };
 
 
